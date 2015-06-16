1ae3e629a705b621e4ccc8cca2849f35be958138
dapias <dandroveros@gmail.com>
2015-06-15 22:13:11 -0500

    Modifiqué input_parameters para que masses y radii sean arreglos.

--- src/input_parameters.jl
+++ src/input_parameters.jl
@@ -7,8 +7,9 @@ Lx1 = 0 #Position at the x-axis of the "left" vertical wall.
 Lx2 = 6.27 #Position at the x-axis of the "right" vertical wall.
 Ly1 = 0.  #Position at the y-axis of the "down" vertical wall.
 Ly2 = 6.27 #Position at the y-axis of the "up" vertical wall.
-vmin = -5.0  #Minimum velocity for the particles.
-vmax = 5.0 #Maximum velocity for the particles.
+etotal = 50.
+radii = [1.,1.,1.,1.,1.]
+masses = [1.,1.,1.,1.,1.]
 tinicial = 0 #Time when simulation starts.
 tmax = 100 #Time when simulation ends
 
