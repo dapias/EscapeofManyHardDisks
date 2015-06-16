6a542766ec600c3ae5537333786bf2c7d1f48f08
dapias <dandroveros@gmail.com>
2015-06-16 11:48:03 -0500

    En run cambié la manera de llamar la función *simulation* de acuerdo a su firma. En input_parameters puse los parámetros necesarios

--- src/run.jl
+++ src/run.jl
@@ -9,5 +9,5 @@ using Visual
 #In this file the main functions of the project are called, i.e. *simulation* from main.jl and *visualizate* from visualization.jl.
 #Additionally the parameters from input_parameters.jl are called.
 
-simulacion = simulation(tinicial, tmax, N, Lx1, Lx2, Ly1, Ly2, vmin, vmax);
+simulacion = simulation(tinicial, tmax, N, Lx1, Lx2, Ly1, Ly2, etotal, masses, radii);
 @time visualize(simulacion, N, Lx1, Lx2, Ly1, Ly2)
