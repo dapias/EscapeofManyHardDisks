378ec4af5ba5b5512c7abe71efca989fe7a9a15c
dapias <dandroveros@gmail.com>
2015-08-06 11:33:11 -0500

    Agregué el parámetro t_max dentro de la firma de la función simulación para que el número de *frames* sea acorde a éste.

--- src/run.jl
+++ src/run.jl
@@ -8,8 +8,9 @@ using Visual
 #In this file the main functions of the project are called, i.e. *simulation* from main.jl and *visualizate* from visualization.jl.
 #Additionally the parameters from input_parameters.jl are called.
 
-radii = ones(5)*r
+radii = ones(N)*r
+masses = ones(N)*r
 h = 0.1*(Lx2 -Lx1 - 2*r)
-#h = 3.
+#h = 2.
 simulacion = simulation(tinicial, tmax, N, Lx1, Lx2, Ly1, Ly2, etotal, masses, radii,r, h);
-@time visualize(simulacion, N, Lx1, Lx2, Ly1, Ly2,r, h)
+@time visualize(simulacion, N, Lx1, Lx2, Ly1, Ly2,r, h, tmax)
